c8290d5ea198c8c69c10f7093f4f9d1ea80704b2
Jan Behrens <dev@jpbehrens.de>
2022-01-13 18:30:05 +0100

    write a class based component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hello World!'
+    }
+  }
+
+  render() {
+    // Toggle SET State 
+    const toggle = () => {
+      const newString = 'Bye Bye World';
+      const toggleString = this.state.string !== newString ? newString : 'Hello World!'
+      
+      // BUILDIN FUNTION TO SET STATE 
+      this.setState({
+        string: toggleString
+      });
+    }
+
+    return (
+      <div className="App" >
+        <header className="App-header">
+          {this.state.string}
+
+          <button onClick={toggle}>Klick mich</button>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
