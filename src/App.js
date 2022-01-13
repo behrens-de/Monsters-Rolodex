e8efbd4ae372c0f341a973ae2352f8b09577a258
Jan Behrens <dev@jpbehrens.de>
2022-01-13 20:03:09 +0100

    updated: map with uniq keys

--- src/App.js
+++ src/App.js
@@ -6,7 +6,12 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello World!'
+      string: 'Hello World!',
+      monsters: [
+        { name: 'Frankenstein', id: 'asc1' },
+        { name: 'Dracula', id: 'asc2' },
+        { name: 'Zombie', id: 'as1w'}
+      ]
     }
   }
 
@@ -15,20 +20,29 @@ class App extends Component {
     const toggle = () => {
       const newString = 'Bye Bye World';
       const toggleString = this.state.string !== newString ? newString : 'Hello World!'
-      
+
       // BUILDIN FUNTION TO SET STATE 
       this.setState({
         string: toggleString
       });
     }
 
+
+
+    // Return = JSX in JSX {S Expresions in this brakets}
+    // JSX Attributes - className - onClick - src
     return (
       <div className="App" >
-        <header className="App-header">
+        {this.state.monsters.map(monster => {
+          // Jedes Element brauch einen Uniq Key damit react sich auf dieses eine Element beim rerendern beziehen kann
+          return (<h1 key={monster.id}>{monster.name}</h1>)
+        })}
+        {/* <header className="App-header">
+          
           {this.state.string}
 
           <button onClick={toggle}>Klick mich</button>
-        </header>
+        </header> */}
       </div>
     );
   }
