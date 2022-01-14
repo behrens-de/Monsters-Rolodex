d08a6a3c6ff0754430ee8cdf5a6826384aebfc05
Jan Behrens <dev@jpbehrens.de>
2022-01-14 18:21:22 +0100

    update

--- src/App.js
+++ src/App.js
@@ -8,48 +8,36 @@ class App extends Component {
     super();
 
     this.state = {
-      string: 'Hello World!',
       monsters: [],
       searchField: '',
-
     }
   }
 
+  // Wenn die Componente das erste mal geladen wird und der State inizialisiert wird
   componentDidMount() {
-    // Wenn die Componente das erste mal geladen wird und der State inizialisiert wird
     fetch('https://jsonplaceholder.typicode.com/users')
       .then(response => response.json())
       .then(users => this.setState({ monsters: users }));
   }
 
+  // Input is change
+  handleChange = e => {
+    this.setState({ searchField: e.target.value });
+  }
+
   render() {
-    // Toggle SET State 
-    // const toggle = () => {
-    //   const newString = 'Bye Bye World';
-    //   const toggleString = this.state.string !== newString ? newString : 'Hello World!'
-
-    //   // BUILDIN FUNTION TO SET STATE 
-    //   this.setState({
-    //     string: toggleString
-    //   });
-    // }
-
     // Filter the Monsters
     const { monsters, searchField } = this.state;
     const filterdMonsters = monsters.filter(monster => {
       return monster.name.toLowerCase().includes(searchField.toLowerCase());
     })
 
-
-
-    // Return = JSX in JSX {S Expresions in this brakets}
-    // JSX Attributes - className - onClick - src
     return (
       <div className="App" >
         <h1>Monster Rolodex</h1>
         <SearchBox
           placeholder={'suche Monster...'}
-          handleChange={e => this.setState({ searchField: e.target.value })}
+          handleChange={this.handleChange}
         />
         <CardList
           monsters={filterdMonsters}
